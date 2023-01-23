bd5d32e4a5a4897eed0097e4b51e381a8d12119d
KubiiCz <jakub.73@hotmail.com>
2023-01-23 11:07:43 +0100

    add prettier config

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,27 +1,32 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
-import { useEffect, useState } from "react";
-import { url } from "inspector";
-import FileUploadSingle from "../components/FileUploadSingle";
-import FileDownload from "../components/FileDownload";
-
-const inter = Inter({ subsets: ["latin"] });
-const axios = require("axios");
+import Head from 'next/head'
+import { Inter } from '@next/font/google'
+import { useEffect, useState } from 'react'
+import FileUploadSingle from '../components/FileUploadSingle'
+import FileDownload from '../components/FileDownload'
+import axios from 'axios'
 
 export default function Home() {
-  const [data, setData] = useState(null);
-  const [isLoading, setLoading] = useState(false);
+  const URL = 'http://localhost:8000/api/main'
+  const [data, setData] = useState(null)
+  const [isLoading, setLoading] = useState(false)
 
-  const getData = async () => {
-    const res = await (await fetch("http://localhost:5000/api/main")).json();
-    setData(res["hello"]);
-  };
+  //Axios GET request
+  const getData = () => {
+    axios
+      .get(URL)
+      .then((res: any) => {
+        // handle success
+        setData(res.data.json())
+      })
+      .catch((error: any) => {
+        // handle error
+        console.log(error)
+      })
+  }
 
   useEffect(() => {
-    getData();
-  }, []);
+    getData()
+  }, [])
 
   return (
     <>
@@ -33,41 +38,36 @@ export default function Home() {
       </Head>
       <main className="max-w-xl mx-auto justify-center content-center">
         <div className="container max-w-7xl self-center mx-auto pt-16">
-          <h1 className="mb-20 text-3xl font-extrabold">CEN Zastropovani cen .xlsx -> .pdf</h1>
-          
-          
-          
+          <h1 className="mb-20 text-3xl font-extrabold">
+            CEN Zastropovani cen .xlsx -{'>'} .pdf
+          </h1>
+
           <div className="wrapper">
-          <h2>1.krok - Vzorový soubor</h2>
-          <FileDownload 
-          fileName="Simple.pdf" 
-          label="Stáhnout"
-          description="Vzorový soubor pro vyplnění dat"
-           />
-          <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+            <h2>1.krok - Vzorový soubor</h2>
+            <FileDownload
+              fileName="Simple.pdf"
+              label="Stáhnout"
+              description="Vzorový soubor pro vyplnění dat"
+            />
+            <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
           </div>
 
-
-
           <div className="wrapper">
-          <h2>2.krok - Nahrát vyplněný soubor</h2>
-          <FileUploadSingle />
-          <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+            <h2>2.krok - Nahrát vyplněný soubor</h2>
+            <FileUploadSingle />
+            <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
           </div>
-          
-          
-          
+
           <div className="wrapper">
-          <h2>3.krok - Stáhnout vyplněný soubor</h2>
-          <FileDownload 
-          fileName="Simple.pdf"
-          label="Download Simple.pdf"
-          description="Stáhněte si vygenerovaný soubor"
-          />
+            <h2>3.krok - Stáhnout vyplněný soubor</h2>
+            <FileDownload
+              fileName="Simple.pdf"
+              label="Download Simple.pdf"
+              description="Stáhněte si vygenerovaný soubor"
+            />
           </div>
         </div>
-
       </main>
     </>
-  );
+  )
 }
